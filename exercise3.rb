a70559edb962c4b94c3b3d754b11d76397690133
Lazigerbill <chaukbill@gmail.com>
2014-08-12 13:41:52 -0400

    exercise3 is completed

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,13 @@
 puts "What is your name?"
-name=gets.strip
-puts "Hi, #{name}!"
+name=gets.chomp
+puts "Hi, #{name.capitalize}! How old are you?"
+age=gets.chomp	
+until age.to_i.is_a?(Integer) && age.to_i >0
+	puts "#{name.capitalize}, that is not a valid age."
+	puts "How old are you?"
+	age=gets.chomp
+end
+
+year_born=Time.new.year-age.to_i
+
+puts "You should be born in #{year_born}"
